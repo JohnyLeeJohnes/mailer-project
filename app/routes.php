50085974de54ae2a378b61ebe52ddc70762c7ab5
Johny <john.le@seznam.cz>
2024-01-28 17:56:44 +0100

    functional PHP mailer!!

--- app/routes.php
+++ app/routes.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 use App\Application\Actions\Email\SendEmailAction;
 use App\Application\Middleware\APIMiddleware;
+use DavidePastore\Slim\Validation\Validation;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
+use Respect\Validation\Validator as v;
 use Slim\App;
 use Slim\Views\PhpRenderer;
 
--- src/Application/Actions/Email/SendEmailAction.php
+++ src/Application/Actions/Email/SendEmailAction.php
@@ -3,12 +3,52 @@
 namespace App\Application\Actions\Email;
 
 use App\Application\Actions\Action;
+use App\Application\Actions\ActionPayload;
+use App\Application\Support\Mailer;
+use Exception;
 use Psr\Http\Message\ResponseInterface as Response;
 
 class SendEmailAction extends Action
 {
     protected function action(): Response
     {
-        return $this->respondWithData(["PRDEL"]);
+        //Validate data from Form
+        $formData = $this->getFormData();
+        if (!$this->validateForm($formData)) {
+            return $this->respond(new ActionPayload(400, 'Invalid Form'));
+        }
+
+        //Create new Mail
+        $mailer = new Mailer();
+        $mailer->mailer->setFrom('pavlat2@spsejecna.cz', 'Jan Pavlát');
+        $mailer->mailer->addAddress($formData['to']);
+        $mailer->mailer->Subject = $formData['subject'];
+        $mailer->mailer->Body = $formData['body'];
+        //If cc exists in the form
+        if (array_key_exists('cc', $formData) && filter_var($formData['cc'], FILTER_VALIDATE_EMAIL)) {
+            $mailer->mailer->addCC($formData['cc']);
+        }
+
+        try{
+            //Send email & handle error
+            $mailer->mailer->send();
+            return $this->respond(new ActionPayload(200, ['message' => 'OK']));
+        } catch (Exception){
+            return $this->respond(new ActionPayload(400, ['message' => 'ERROR']));
+        }
+    }
+
+    private function validateForm(array $formData): bool
+    {
+        if (!array_key_exists('to', $formData) || !filter_var($formData['to'], FILTER_VALIDATE_EMAIL)) {
+            return false;
+        }
+        if (!array_key_exists('subject', $formData)) {
+            return false;
+        }
+        if (!array_key_exists('body', $formData)) {
+            return false;
+        }
+        return true;
     }
 }
